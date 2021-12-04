9de6156100c492baaa335b5899987daac164cfe0
Rafael <rafael@petize.com.br>
2021-12-04 12:32:33 -0300

    Commit inicial do projeto.

---
+++ src/main/java/br/com/giantlivros/livrariaonline/livrariaonline/Application.java
@@ -0,0 +1,13 @@
+package br.com.giantlivros.livrariaonline.livrariaonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
