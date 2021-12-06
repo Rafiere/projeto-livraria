9d60a55a2cf9a72fd7a8f2439d58c9da6cee9345
Rafael <rafael@petize.com.br>
2021-12-06 11:19:45 -0300

    fix: desabilita CSRF do Spring Security.

--- application/src/main/java/br/com/giantlivros/livrariaonline/Application.java
+++ application/src/main/java/br/com/giantlivros/livrariaonline/Application.java
@@ -1,7 +1,12 @@
 package br.com.giantlivros.livrariaonline;
 
+import br.com.giantlivros.livrariaonline.produto.domain.ProdutoLeitura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 @SpringBootApplication
 public class Application {
