714139defd5f10b6cf3b5efbdf1e0604e3cd1130
Rafael <rafael@petize.com.br>
2021-12-06 11:19:18 -0300

    fix: Insere e modifica configurações e dependências.

--- application/src/main/java/br/com/giantlivros/livrariaonline/config/SwaggerConfig.java
+++ application/src/main/java/br/com/giantlivros/livrariaonline/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package br.com.giantlivros.livrariaonline.docs;
+package br.com.giantlivros.livrariaonline.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
